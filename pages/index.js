e0ef901f8988bff51894f24c9a720bc1e2582f65
Brandon Arias <2017030706@upsin.edu.mx>
2024-11-11 16:59:16 -0700

    Latest products created

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import NewProducts from "@/components/NewProducts";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
-export default function HomePage({ product }) {
+export default function HomePage({ featuredProduct, newProducts }) {
   return (
     <div>
       <Header />
-      <Featured product={product} />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
     </div>
   );
 }
@@ -15,8 +17,15 @@ export default function HomePage({ product }) {
 export async function getServerSideProps() {
   const featuredProductId = "671d2ee1269985c60774d911";
   mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
   return {
-    props: { product: JSON.parse(JSON.stringify(product)) },
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
