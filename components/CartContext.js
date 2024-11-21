781a24fd865c9ccb6c41bb6fbfdfef3e68bc0949
Brandon Arias <2017030706@upsin.edu.mx>
2024-11-21 10:01:10 -0700

    Show orders created

--- components/CartContext.js
+++ components/CartContext.js
@@ -5,13 +5,11 @@ export const CartContext = createContext({});
 export function CartContextProvider({ children }) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
   const [cartProducts, setCartProducts] = useState([]);
-
   useEffect(() => {
     if (cartProducts?.length > 0) {
-      ls.setItem("cart", JSON.stringify(cartProducts));
+      ls?.setItem("cart", JSON.stringify(cartProducts));
     }
   }, [cartProducts]);
-
   useEffect(() => {
     if (ls && ls.getItem("cart")) {
       setCartProducts(JSON.parse(ls.getItem("cart")));
@@ -26,13 +24,22 @@ export function CartContextProvider({ children }) {
       if (pos !== -1) {
         return prev.filter((value, index) => index !== pos);
       }
-
       return prev;
     });
   }
+  function clearCart() {
+    setCartProducts([]);
+    ls.removeItem("cart");
+  }
   return (
     <CartContext.Provider
-      value={{ cartProducts, setCartProducts, addProduct, removeProduct }}
+      value={{
+        cartProducts,
+        setCartProducts,
+        addProduct,
+        removeProduct,
+        clearCart,
+      }}
     >
       {children}
     </CartContext.Provider>
--- pages/cart.js
+++ pages/cart.js
@@ -1,16 +1,19 @@
-import Button from "@/components/Button";
-import { CartContext } from "@/components/CartContext";
-import Center from "@/components/Center";
 import Header from "@/components/Header";
-import Input from "@/components/Input";
-import Table from "@/components/Table";
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
+import Center from "@/components/Center";
+import Button from "@/components/Button";
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "@/components/CartContext";
+import axios from "axios";
+import Table from "@/components/Table";
+import Input from "@/components/Input";
 
 const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1.2fr 0.8fr;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1.2fr 0.8fr;
+  }
   gap: 40px;
   margin-top: 40px;
 `;
@@ -26,22 +29,36 @@ const ProductInfoCell = styled.td`
 `;
 
 const ProductImageBox = styled.div`
-  width: 100px;
+  width: 70px;
   height: 100px;
-  padding: 10px;
+  padding: 2px;
   border: 1px solid rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
   img {
-    max-width: 80px;
-    max-height: 80px;
+    max-width: 60px;
+    max-height: 60px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 10px;
+    width: 100px;
+    height: 100px;
+    img {
+      max-width: 80px;
+      max-height: 80px;
+    }
   }
 `;
 
 const QuantityLabel = styled.span`
-  padding: 0 3px;
+  padding: 0 15px;
+  display: block;
+  @media screen and (min-width: 768px) {
+    display: inline-block;
+    padding: 0 10px;
+  }
 `;
 
 const CityHolder = styled.div`
@@ -50,7 +67,8 @@ const CityHolder = styled.div`
 `;
 
 export default function CartPage() {
-  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct, clearCart } =
+    useContext(CartContext);
   const [products, setProducts] = useState([]);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -58,6 +76,7 @@ export default function CartPage() {
   const [postalCode, setPostalCode] = useState("");
   const [streetAddress, setStreetAddress] = useState("");
   const [country, setCountry] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
   useEffect(() => {
     if (cartProducts.length > 0) {
       axios.post("/api/cart", { ids: cartProducts }).then((response) => {
@@ -67,6 +86,15 @@ export default function CartPage() {
       setProducts([]);
     }
   }, [cartProducts]);
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+    if (window?.location.href.includes("success")) {
+      setIsSuccess(true);
+      clearCart();
+    }
+  }, []);
   function moreOfThisProduct(id) {
     addProduct(id);
   }
@@ -93,7 +121,7 @@ export default function CartPage() {
     total += price;
   }
 
-  if (window.location.href.includes("success")) {
+  if (isSuccess) {
     return (
       <>
         <Header />
@@ -101,7 +129,7 @@ export default function CartPage() {
           <ColumnsWrapper>
             <Box>
               <h1>Thanks for your order!</h1>
-              <p>We will email when your order will be sent.</p>
+              <p>We will email you when your order will be sent.</p>
             </Box>
           </ColumnsWrapper>
         </Center>
@@ -211,7 +239,7 @@ export default function CartPage() {
                 name="country"
                 onChange={(ev) => setCountry(ev.target.value)}
               />
-              <Button black="true" block="true" onClick={goToPayment}>
+              <Button black block onClick={goToPayment}>
                 Continue to payment
               </Button>
             </Box>
